3cf8799baa2c15bb472252e47a7b1662f05b374d
sanmathibs <sanmathi@blueskycreations.com.au>
2023-06-27 10:36:43 +1000

    Modified

--- app.py
+++ app.py
@@ -9,9 +9,9 @@ from flask import Flask, render_template, request
 from tensorflow.keras.preprocessing.image import load_img
 from tensorflow.keras.preprocessing.image import img_to_array
 
-app = Flask(__name__)
-
 global model
+
+app = Flask(__name__)
 
 
 def load_image(img_path):
@@ -44,20 +44,35 @@ def prediction(img_path):
         return "Good"
 
 
-if __name__ == "__main__":
+
+# routes
+@app.route("/", methods=['GET', 'POST'])
+def main():
+    return render_template("home.html")
+
+
+@app.route("/about")
+def about_page():
+    return "Please subscribe  Artificial Intelligence Hub..!!!"
+
+
+@app.route("/submit", methods=['GET', 'POST'])
+def get_output():
+    if request.method == 'POST':
+        img = request.files['my_image']
+
+        img_path = "static/" + img.filename
+        img.save(img_path)
+
+        p = prediction(img_path)
+
+    return render_template("home.html", prediction=p, img_path=img_path)
+
+
+if __name__ == '__main__':
     # Load the model
     model = load_model('model.h5')
     print("Model is loaded")
 
-    # Model prediction
-    image_path = r"C:\Users\sanma\PycharmProjects\BCDetection\static\bad (24)k.jpeg"
-    img = load_image(image_path)
-    k = model.predict(img)
-    labels = np.array(k)
-
-    labels[labels >= 0.6] = 1
-    labels[labels < 0.6] = 0
-
-    print(k)
-    if labels[labels > 0.7]:
-        print("Gooddd")
+    # app.debug = True
+    app.run(debug=True)
